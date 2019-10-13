6d6fd74d0134f4af5d4081f895f33d51035adf81
kiran gunana <gunana.kiran@gmail.com>
2019-10-13 08:07:47 -0700

    cfg push

---
+++ netmiko_cfg_change/cfg_list_int_desc_connect.py
@@ -0,0 +1,17 @@
+from netmiko import ConnectHandler
+from  getpass import getpass
+
+device1 = {
+    "host" : 'cisco3.lasthop.io',
+    "username" : 'pyclass',
+    "password" : getpass(),
+    "device_type" : 'cisco_ios',
+    #session_log = 'my_session.txt'
+}
+
+net_connect = ConnectHandler(**device1)
+print(net_connect.find_prompt())
+cfg = [ 'logging buffered 20000', 'int gi0/0/0', 'desc test1' ]
+output = net_connect.send_config_set(cfg)
+print(output)
+net_connect.disconnect()
