43fb91a9298d95b7178c2d65e6a672dd9d594642
kiran gunana <gunana.kiran@gmail.com>
2019-10-15 19:21:48 -0700

    netmi_scp

---
+++ netmiko_scp/scp_get_connect.py
@@ -0,0 +1,29 @@
+from netmiko import ConnectHandler, file_transfer
+from  getpass import getpass
+
+device1 = {
+    "host" : 'cisco3.lasthop.io',
+    "username" : 'pyclass',
+    "password" : getpass(),
+    "device_type" : 'cisco_ios',
+    #session_log = 'my_session.txt'
+}
+
+source_file = "testx.txt"
+dest_file = "testaa.txt"
+direction = "get"
+file_system = "bootflash:"
+
+# create the netmiko ssh conenction
+ssh_conn  = ConnectHandler(**device1)
+transfer_dict = file_transfer(
+    ssh_conn,
+    source_file = source_file,
+    dest_file = dest_file,
+    file_system = file_system,
+    direction = direction,
+    overwrite_file = True,
+)
+print(transfer_dict)
+ssh_conn.disconnect()
+
